1578be7194ad23fab94f69a068856829f8a3e280
Nhan Phan <kippulainenko@gmail.com>
2022-06-15 14:36:46 +0300

    type function types and callbacks

--- app.ts
+++ app.ts
@@ -1,31 +1,30 @@
-type Combinabel = number | string;
-type COnversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-    input1: Combinabel, 
-    input2: Combinabel,
-    // literal types
-    resultConversion: COnversionDescriptor) {
-    let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    return result
-
-    // if (resultConversion === 'as-number') {
-    //     return +result;
-    // } else {
-    //     return result.toString();
-    // }
+function add(n1: number, n2: number) {
+    return n1 + n2
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+function printResult(num: number): void {
+    console.log('Result: ' + num);
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
+printResult(add(5, 12));
 
-const combinedNames = combine('Max', 'Anna', 'as-text')
-console.log(combinedNames);
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
+// let someValue: undefined;
+
+//-----//
+
+function addAddHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result)
+}
+
+addAddHandle(10, 20, (result) => {
+    console.log(result)
+})
+
