3d16b164f895a6666d5ae9ccb4cfb8747935fd78
Nhan Phan <kippulainenko@gmail.com>
2022-06-17 22:32:24 +0300

    type index properties

--- src/app.ts
+++ src/app.ts
@@ -105,7 +105,25 @@ function moveAnimal(animal: Animal) {
 
 moveAnimal({type: 'bird', flyingSpeed: 10})
 
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+const userInputElement = document.getElementById('user-input');
 
+if(userInputElement) {
+    (userInputElement as HTMLInputElement).value= 'Hi there!'
+}
+// we create objects where we have some error identifier, which could be the idea of the input field to which the error belongs
+interface ErrorContainer { // {email: 'Not a valid email', username: 'Must start with a character'}
+    // id: string;
+    [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+    email: 'Not a valid email!',
+    username: 'Must start with a capital character!'
+};
+
+
+//------//
 interface Admins {
     name: string;
     privileges: string[];
