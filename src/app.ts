0a2d2320c270a8a7f606031bf40dd130485184c8
Nhan Phan <kippulainenko@gmail.com>
2022-06-18 18:08:24 +0300

    type generic classes

--- src/app.ts
+++ src/app.ts
@@ -34,3 +34,48 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 console.log(countAndDescribe('Hi there!'));
 console.log(countAndDescribe(['Olala']));
 console.log(countAndDescribe([]));
+
+// access a property in an object []
+function extractAndCovert<T extends object, U extends keyof T>(obj: T, key: U) {
+    return 'Value' + obj[key]
+}
+
+extractAndCovert({name: 'Max'}, 'name');
+
+//-----Generic Classes ------ //
+
+
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) { 
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        if(this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>()
+textStorage.addItem('Nio');
+textStorage.addItem('Manu');
+textStorage.removeItem('Manu');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const maxObj = {name: 'Nio'}
+// objStorage.addItem(maxObj);
+// objStorage.addItem({name: 'Manu'});
+// //..
+// objStorage.removeItem(maxObj);
+// console.log(objStorage.getItems());
