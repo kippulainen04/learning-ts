4ad5d92104f0b2b628d7401ea0803c0072251f73
Nhan Phan <kippulainenko@gmail.com>
2022-06-18 20:29:27 +0300

    use decorator factories

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
-function Logger(constructor: Function) {
-    console.log('Logging....');
-    console.log(constructor)
+function Logger(logString: string) {
+    return function (constructor: Function) {
+        console.log(logString);
+        console.log(constructor)
+    }
 }
 
-@Logger
+function WithTempalte(template: string, hookId: string) {
+    return function(constructor: any) {
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if(hookEl) {
+            hookEl.innerHTML = template
+            hookEl.querySelector('h1')!.textContent = p.name
+        }
+    }
+}
+
+// @Logger('LOGGIN - PERSON')
+@WithTempalte('<h1>My Person Object</h1>', 'app')
 class Person {
     name = 'Max';
 
